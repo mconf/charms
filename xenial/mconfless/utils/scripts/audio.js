fdb9f72b17eaf3985461daeeaeae248d77c7398e
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2018-08-03 15:09:24 -0300

    Delay must return a Promise

--- xenial/mconfless/utils/scripts/audio.js
+++ xenial/mconfless/utils/scripts/audio.js
@@ -9,7 +9,7 @@ const selectorTimeout = 60000 // 60 seconds
 const url = HOST + '/demo/demoHTML5.jsp?action=create' +
     '&username=Boty+McBotface' +
     '&meetingname=' + encodeURI(ROOM)
-const delay = ms => { new Promise(resolve => setTimeout(resolve, ms)) }
+const delay = ms => { return new Promise(resolve => setTimeout(resolve, ms)) }
 
 async function click(page, element) {
   await page.waitForSelector(element, { timeout: selectorTimeout })
