070f0056d79862c3c0c6f52bd4365e4083cb7cc3
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2018-10-02 16:31:00 -0300

    Fixed TypeError when calling the delay function

--- xenial/mconfless/utils/scripts/core/util.js
+++ xenial/mconfless/utils/scripts/core/util.js
@@ -10,18 +10,19 @@ const config = conf.config
 const timeout = config.timeout
 const url = config.url
 
+const delay = async time => new Promise(resolve => setTimeout(resolve, time))
 const token = () => Math.random().toString(36).substring(2, 15)
 
 module.exports = {
   url: url.host + url.demo + url.userTag + encodeURI(url.user) + url.meetingTag + encodeURI(url.meeting),
-  delay: async time => new Promise(resolve => setTimeout(resolve, time)),
+  delay: delay,
   click: async (page, element, relief = false) => {
-    if (relief) await this.delay(timeout.relief)
+    if (relief) await delay(timeout.relief)
     await page.waitForSelector(element, { timeout: timeout.selector })
     await page.click(element)
   },
   type: async (page, element, text, relief = false) => {
-    if (relief) await this.delay(timeout.relief)
+    if (relief) await delay(timeout.relief)
     await page.waitForSelector(element, { timeout: timeout.selector })
     await page.type(element, text)
   },
