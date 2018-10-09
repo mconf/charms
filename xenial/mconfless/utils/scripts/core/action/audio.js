bbdeff34c192fe9635451468aee69730fb17f2dc
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2018-10-09 12:18:41 -0300

    Extra audio actions

--- xenial/mconfless/utils/scripts/core/action/audio.js
+++ xenial/mconfless/utils/scripts/core/action/audio.js
@@ -10,12 +10,14 @@ const util = require('../util.js')
 const audio = conf.label.audio
 
 module.exports = {
-  close: async page => await util.click(page, audio.dialog.close),
-  join: async page => {
+  join: async page => await util.click(page, audio.join),
+  microphone: async page => {
     await util.click(page, audio.dialog.microphone)
     await util.click(page, audio.echo.confirm)
   },
   listen: async page => await util.click(page, audio.dialog.listenOnly),
-  mute: async page => {},
-  leave: async page => {}
+  close: async page => await util.click(page, audio.dialog.close),
+  mute: async page => await util.click(page, audio.mute, true),
+  unmute: async page => await util.click(page, audio.unmute, true),
+  leave: async page => await util.click(page, audio.leave, true)
 }
