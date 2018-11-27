b22c6a1a8c746aabcdd4cea53bb4861b89da4190
Pedro Beschorner Marin <pedrobmarin@gmail.com>
2018-11-27 18:48:01 -0200

    Included whiteboard actions

--- xenial/mconfless/utils/scripts/core/action/whiteboard.js
+++ xenial/mconfless/utils/scripts/core/action/whiteboard.js
@@ -8,6 +8,24 @@ const conf = require('../conf.js')
 const util = require('../util.js')
 
 const whiteboard = conf.label.whiteboard
+const data = conf.config.data
+
+const point = box => ({ x: box.x + (Math.random() * box.width), y: box.y + (Math.random() * box.height) })
+
+const draw = async (page, tool) => {
+  await util.click(page, whiteboard.tools.open, true)
+  await util.click(page, tool, true)
+
+  const board = await page.$(whiteboard.board)
+  const box = await board.boundingBox()
+
+  const start = point(box)
+  await page.mouse.move(start.x, start.y)
+  await page.mouse.down()
+  const finish = point(box)
+  await page.mouse.move(finish.x, finish.y, { steps: data.whiteboard.steps })
+  await page.mouse.up()
+}
 
 const evaluate = {
   text: {
@@ -51,28 +69,37 @@ module.exports = {
     await util.test(page, evaluate.text)
   },
   line: async page => {
-    await util.click(page, whiteboard.tools.open)
-    await util.click(page, whiteboard.tools.line, true)
+    await draw(page, whiteboard.tools.line)
     await util.test(page, evaluate.line)
   },
   ellipse: async page => {
-    await util.click(page, whiteboard.tools.open)
-    await util.click(page, whiteboard.tools.ellipse, true)
+    await draw(page, whiteboard.tools.ellipse)
     await util.test(page, evaluate.ellipse)
   },
   triangle: async page => {
-    await util.click(page, whiteboard.tools.open)
-    await util.click(page, whiteboard.tools.triangle, true)
+    await draw(page, whiteboard.tools.triangle)
     await util.test(page, evaluate.triangle)
   },
   rectangle: async page => {
-    await util.click(page, whiteboard.tools.open)
-    await util.click(page, whiteboard.tools.rectangle, true)
+    await draw(page, whiteboard.tools.rectangle)
     await util.test(page, evaluate.rectangle)
   },
   pencil: async page => {
     await util.click(page, whiteboard.tools.open)
     await util.click(page, whiteboard.tools.pencil, true)
+
+    const points = data.whiteboard.points
+    const board = await page.$(whiteboard.board)
+    const box = await board.boundingBox()
+
+    let p = point(box)
+    await page.mouse.move(p.x, p.y)
+    await page.mouse.down()
+    for (let i = 0; i < points; i++) {
+      p = point(box)
+      await page.mouse.move(p.x, p.y, { steps: data.whiteboard.steps })
+    }
+    await page.mouse.up()
     await util.test(page, evaluate.pencil)
   },
   undo: async page => {
