9b3c73fd1544f7f6d4aac9988978e1548324a2e0
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2021-05-11 16:21:22 -0300

    fix: add support for encryped OCI privkeys

--- xenial/mconfless/provision/terraform-oci/provider.tf
+++ xenial/mconfless/provision/terraform-oci/provider.tf
@@ -3,5 +3,6 @@ provider "oci" {
   user_ocid = var.oci_user_ocid
   fingerprint = var.oci_fingerprint
   private_key_path = var.oci_private_key_path
+  private_key_password = var.oci_private_key_password
   disable_auto_retries = false
 }
